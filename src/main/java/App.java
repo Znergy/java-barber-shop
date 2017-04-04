1ac058dd5d97259b791ded096bb53cd564179535
Ryan <ryanjonesirl@gmail.com>
2017-04-04 11:18:20 -0700

    add a post route for the clients form and fix a minor issue with stylist/:id/client/new, there was an issue with the :id not being passed due to my route not saving to the database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,6 +27,7 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("name");
       Stylist stylist = new Stylist(name);
+      stylist.save();
       model.put("stylist", stylist);
       model.put("template", "templates/stylist.vtl");
       return new ModelAndView(model, layout);
@@ -45,6 +46,18 @@ public class App {
       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylistid")));
       model.put("stylist", stylist);
       model.put("template", "templates/client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/client", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String description = request.queryParams("description");
+      int id_stylist = Integer.parseInt(request.queryParams("id_stylist"));
+      Stylist stylist = new Stylist(name, description, id_stylist);
+      stylist.save();
+      model.put("stylist", stylist);
+      model.put("template", "templates/client.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
